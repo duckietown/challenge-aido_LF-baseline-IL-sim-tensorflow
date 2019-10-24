e3b3f456f5a727de0d710b41794a8eb6da6a50d7
Andrea Censi <acensi@ethz.ch>
2019-10-24 21:34:49 +0200

    fix interval

--- submission/solution.py
+++ submission/solution.py
@@ -36,7 +36,8 @@ class TensorflowTemplateAgent:
 
     def on_received_get_commands(self, context: Context):
         pwm_left, pwm_right = self.compute_action(self.current_image)
-
+        pwm_left = float(np.clip(pwm_left, -1, +1))
+        pwm_right = float(np.clip(pwm_right, -1, +1))
         grey = RGB(0.0, 0.0, 0.0)
         led_commands = LEDSCommands(grey, grey, grey, grey, grey)
         pwm_commands = PWMCommands(motor_left=pwm_left, motor_right=pwm_right)
